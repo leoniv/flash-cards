578945d9249f8d01ac363fe2b3edb82ec4aaab72
Leonid Vlasov <leoniv.vlasov@gmail.com>
2025-09-19 22:03:59 +0700

    fix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,31 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  isCI = (builtins.getEnv "CI") == "true";
+  isCIs = if isCI then "true" else "false";
+in
 pkgs.mkShell {
   packages = with pkgs; [
-    go
     nodejs
     nodePackages.typescript
-    nodePackages.eslint
-    nodePackages.prettier
-    nodePackages.typescript-language-server
-  ];
+  ] ++ ( if isCI then [
+      cacert
+    ] else [
+      nodePackages.eslint
+      nodePackages.prettier
+      nodePackages.typescript-language-server
+    ]
+  );
 
   shellHook = ''
+    echo isCI = ${isCIs}
+  '' + ( if isCI then ''
+    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    export NIX_SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    export NODE_OPTIONS="--use-openssl-ca $NODE_OPTIONS"
+  '' else ''
     alias yarn="corepack yarn"
     alias pnpm="corepack pnpm"
-    echo "Go is ready! Install packages with: go install <package>@latest"
-  '';
+  ''
+  );
 }
