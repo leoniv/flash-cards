688fbf9fea0d932b98f3cc3e6d46494c1d99e2ad
Leonid Vlasov <leoniv.vlasov@gmail.com>
2025-10-04 14:39:13 +0700

    Fix lsp conf

--- .nvim.lua
+++ .nvim.lua
@@ -1,8 +1,1 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.ts_ls.setup{
-  cmd = { vim.fn.exepath("typescript-language-server"), "--stdio" },
-  root_dir = lspconfig.util.root_pattern("tsconfig.json", "package.json", ".git"),
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx",
-                "typescript", "typescriptreact", "typescript.tsx" },
-}
+vim.lsp.enable('ts_ls')
